f785c6b086833ba1df1d3c0aa5370c002b5fe98e
Feanen <feanen.elvenwarrior@gmail.com>
2023-12-25 17:11:20 +0200

    Commit 4. Fixed an error with connecting to correct databases in project. Created two constuctors for developers for adding information to databases. Added some info to Techniques table

---
+++ Models/Technique.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PsychTestsMilitary.Models
+{
+    public class Technique
+    {
+        [Key] public int id { get; set; }
+
+        private string name, instruction;
+
+        public Technique() { }
+        public Technique(int id, string name, string instruction) 
+        { 
+            this.id = id;
+            this.name = name;
+            this.instruction = instruction; 
+        }
+        public int Id
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public string Instruction
+        {
+            get { return this.instruction; }
+            set { this.instruction = value; }
+        }
+
+    }
+}
---
+++ Services/Contexts/TechniquesContext.cs
@@ -0,0 +1,17 @@
+﻿using PsychTestsMilitary.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PsychTestsMilitary.Services.Contexts
+{
+    public class TechniquesContext : DbContext
+    {
+        public DbSet<Technique> Techniques { get; set; }
+        public TechniquesContext() : base("TechniquesConnection") {}
+      
+    }
+}
