19874679eb3b7b6aad32519bd974386920ab9891
Feanen <feanen.elvenwarrior@gmail.com>
2023-12-29 15:52:31 +0200

    Commit 5. Added view and functionality to constuctor with adding questions to database. Created view for choosing developer tools. Added questions to database from first psych.technique

---
+++ Models/Question.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PsychTestsMilitary.Models
+{
+    public class Question
+    {
+        [Key] public int qId { get; set; }
+        public int Technique_id { get; set; }
+        public int Number { get; set; }
+        public string Description { get; set; }
+
+        public Question() { }
+        public Question(int id, int number, string description, int tech_id)
+        {
+            qId = id;
+            Number = number;
+            Description = description;
+            Technique_id = tech_id;
+        }
+    }
+}
